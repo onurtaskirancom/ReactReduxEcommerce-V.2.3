4c6623a56c34fcd819614b04dc167c10ba55eda9
onurtaskirancom <taskiranonur9@gmail.com>
2022-10-25 18:53:38 +0300

    Successful Payment

---
+++ client/src/functions/stripe.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const createPaymentIntent = (authtoken) =>
+  axios.post(
+    `${process.env.REACT_APP_API}/create-payment-intent`,
+    {},
+    {
+      headers: {
+        authtoken,
+      },
+    }
+  );
+    
