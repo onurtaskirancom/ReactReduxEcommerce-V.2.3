2958a3ea25e27d1f53dda404987ee3bb748c59c9
onurtaskirancom <taskiranonur9@gmail.com>
2022-11-03 12:28:39 +0300

    User Orders Backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,6 +12,7 @@ const {
   saveAddress,
   applyCouponToUserCart,
   createOrder,
+  orders,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
@@ -19,7 +20,8 @@ router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
 
-router.post('/user/order', authCheck, createOrder)
+router.post('/user/order', authCheck, createOrder);
+router.get("/user/orders", authCheck, orders);
 
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
