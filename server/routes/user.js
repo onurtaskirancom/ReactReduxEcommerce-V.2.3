81708d8456100f55cdea30ecd7475bdd8d917ab6
onurtaskirancom <taskiranonur9@gmail.com>
2022-11-16 09:31:12 +0300

    Wishlist Functions Frontend

--- server/routes/user.js
+++ server/routes/user.js
@@ -13,6 +13,9 @@ const {
   applyCouponToUserCart,
   createOrder,
   orders,
+  addToWishlist,
+  wishlist,
+  removeFromWishlist,
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); // save cart
@@ -20,17 +23,16 @@ router.get("/user/cart", authCheck, getUserCart); // get cart
 router.delete("/user/cart", authCheck, emptyCart); // empty cart
 router.post("/user/address", authCheck, saveAddress);
 
-router.post('/user/order', authCheck, createOrder);
+router.post("/user/order", authCheck, createOrder);
 router.get("/user/orders", authCheck, orders);
 
 // coupon
 router.post("/user/cart/coupon", authCheck, applyCouponToUserCart);
 
-// whistlist 
-router.post('/user/wishlist', authCheck, addToWishlist)
-router.get('/user/wishlist', authCheck, withlist)
-router.put('/user/wishlist/:productId', authCheck, removeFromWishlist);
-
+// wishlist
+router.post("/user/wishlist", authCheck, addToWishlist);
+router.get("/user/wishlist", authCheck, wishlist);
+router.put("/user/wishlist/:productId", authCheck, removeFromWishlist);
 
 // router.get("/user", (req, res) => {
 //   res.json({
