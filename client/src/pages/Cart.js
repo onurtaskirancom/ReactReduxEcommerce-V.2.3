0f0f493b158e848ab81096fc6b905efc3b86c3e5
onurtaskirancom <taskiranonur9@gmail.com>
2022-11-18 12:07:13 +0300

    Cash On Delivery

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -24,18 +24,32 @@ const Cart = ({ history }) => {
       .catch((err) => console.log("cart save err", err));
   };
 
+  const saveCashOrderToDb = () => {
+    // console.log("cart", JSON.stringify(cart, null, 4));
+    dispatch({
+      type: "COD",
+      payload: true,
+    });
+    userCart(cart, user.token)
+      .then((res) => {
+        console.log("CART POST RES", res);
+        if (res.data.ok) history.push("/checkout");
+      })
+      .catch((err) => console.log("cart save err", err));
+  };
+
   const showCartItems = () => (
-    <table className='table table-bordered'>
-      <thead className='thead-light'>
+    <table className="table table-bordered">
+      <thead className="thead-light">
         <tr>
-          <th scope='col'>Image</th>
-          <th scope='col'>Title</th>
-          <th scope='col'>Price</th>
-          <th scope='col'>Brand</th>
-          <th scope='col'>Color</th>
-          <th scope='col'>Count</th>
-          <th scope='col'>Shipping</th>
-          <th scope='col'>Remove</th>
+          <th scope="col">Image</th>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+          <th scope="col">Remove</th>
         </tr>
       </thead>
 
@@ -46,20 +60,20 @@ const Cart = ({ history }) => {
   );
 
   return (
-    <div className='container-fluid pt-2'>
-      <div className='row'>
-        <div className='col-md-8'>
+    <div className="container-fluid pt-2">
+      <div className="row">
+        <div className="col-md-8">
           <h4>Cart / {cart.length} Product</h4>
 
           {!cart.length ? (
             <p>
-              No products in cart. <Link to='/shop'>Continue Shopping.</Link>
+              No products in cart. <Link to="/shop">Continue Shopping.</Link>
             </p>
           ) : (
             showCartItems()
           )}
         </div>
-        <div className='col-md-4'>
+        <div className="col-md-4">
           <h4>Order Summary</h4>
           <hr />
           <p>Products</p>
@@ -74,15 +88,25 @@ const Cart = ({ history }) => {
           Total: <b>${getTotal()}</b>
           <hr />
           {user ? (
-            <button
-              onClick={saveOrderToDb}
-              className='btn btn-sm btn-primary mt-2'
-              disabled={!cart.length}
-            >
-              Proceed to Checkout
-            </button>
+            <>
+              <button
+                onClick={saveOrderToDb}
+                className="btn btn-sm btn-primary mt-2"
+                disabled={!cart.length}
+              >
+                Proceed to Checkout
+              </button>
+              <br />
+              <button
+                onClick={saveCashOrderToDb}
+                className="btn btn-sm btn-warning mt-2"
+                disabled={!cart.length}
+              >
+                Pay Cash on Delivery
+              </button>
+            </>
           ) : (
-            <button className='btn btn-sm btn-primary mt-2'>
+            <button className="btn btn-sm btn-primary mt-2">
               <Link
                 to={{
                   pathname: "/login",
